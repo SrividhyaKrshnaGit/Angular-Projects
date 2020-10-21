8e339ade8ef9ac6082bd8560dc0b71004483cfd5
Srividhya Krshna <Srividhya.krshna@rntbci.com>
2020-10-21 07:22:14 +0530

    Form Project

---
+++ src/app/user.ts
@@ -0,0 +1,11 @@
+export class USer {
+    constructor(
+        //Typescript short hand syntax
+        public name: string,
+        public email: string,
+        pubic phone: number,
+        public topic: String,
+        public timePreference:String,
+        public subscribe:boolean
+    ){}
+}
